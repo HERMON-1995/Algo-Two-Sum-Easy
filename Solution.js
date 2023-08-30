325d88f8af0950d42fb04375f591154db735e804
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-08-30 18:16:25 +0300

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,1 +1,9 @@
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+       if (nums[i] + nums[i+1] === target) {
+          return [i, i+1]
+       }
+    }
+};
 
+console.log(twoSum([3,2,4], 6));
